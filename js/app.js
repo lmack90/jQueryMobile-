89d5788483c4e70450a21a040d7c524f5a3ce2a1
lmack90 <lisamacken@Lisas-MacBook-Pro.local>
2017-03-04 16:35:44 +0000

    jQuery Mobile with quiz
    Making a jQuery html based app with quiz functions

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+/* Load splash screen */
+
+$(function() {
+  setTimeout(hideSplash, 2000);
+});
+
+function hideSplash() {
+  $.mobile.changePage("#pageone", "fade");
+}
+
+
+/* global $,document,console,quizMaster */
+$(document).ready(function() {
+	
+	$(document).on("pageshow", "#quizPage", function() {
+		console.log("Page show");
+		//initialize the quiz
+		quizMaster.execute("q1.json", ".quizdisplay", function(result) {
+			console.log("SUCESS CB");
+			console.dir(result);	
+		});
+	});
+
+});
